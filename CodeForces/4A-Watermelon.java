032f6a958ffd3caad0d13938e7cfb228ff972393
Sinni Singla <sinnisingla@outlook.com>
2024-06-15 15:08:23 +0530

    codeforces: 4a - watermelon

---
+++ CodeForces/4A-Watermelon.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        Integer weight;
+        
+        weight = Integer.parseInt(inputScanner.nextLine());  
+        inputScanner.close(); 
+
+        if(weight%2 == 0 && weight >= 4) {
+            System.out.println("Yes");
+            return;
+        }
+
+        System.out.println("No");
+    }
+}
