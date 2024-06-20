f90112c2e61dd54b89f321a662e06a21dbe97364
Sinni Singla <sinnisingla@outlook.com>
2024-06-20 23:39:40 +0530

    feat(epiForJava): swap the bits

---
+++ EPIForJava/IsDecimalPalindrome.java
@@ -0,0 +1,29 @@
+package EPIForJava;
+
+import java.util.Scanner;
+
+public class IsDecimalPalindrome {
+    public static long getReverseOfNumber(long num) {
+        long result = 0;
+
+        while(num != 0) {
+            result = result * 10 + (num % 10) ;
+            num = num/10;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num = scanner.nextLong();
+        long reverseNum = getReverseOfNumber(num);
+        System.out.println(reverseNum);
+        if(num == reverseNum) {
+            System.out.println("Provided number is a palindrome!");
+            return;
+        }
+
+        System.out.println("Provided number is not a palindrome!");
+    }
+}
--- EPIForJava/README.md
+++ EPIForJava/README.md
@@ -1,3 +1,5 @@
 ### Chapter 5: Primitive Types
 
 - [Find Parity of a number](ParityOfANumber.java)
+- [Swap The Bits](SwapTheBits.java)
+- [Check If a number is a palindrome](IsDecimalPalindrome.java)
